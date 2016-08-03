0da49685e8b47dcc61f3d2ec7b5a8b4d80546af0
Aaron J. Lang <aaronjameslang@googlemail.com>
2016-08-03 18:20:49 +0100

    Get digester and tests working

---
+++ src/digest.js
@@ -0,0 +1,44 @@
+module.exports = {
+  digestWord: digestWord,
+  incrementSubmemoir: incrementSubmemoir
+}
+
+/**
+ * @param word String
+ */
+function digestWord (memoir, word) {
+  if (!memoir) throw new Error
+  if (!word) throw new Error
+  var link = ['START', 'START', 'START']
+  for (let i = 0; i < word.length; i += 1) {
+    let letter = word[i]
+    link.shift()
+    link.push(letter)
+    incrementSubmemoir(memoir, link)
+  }
+
+  link.shift()
+  link.push('END')
+
+  incrementSubmemoir(memoir, link)
+}
+
+/**
+ * @param memoir array
+ * @param link array
+ */
+function incrementSubmemoir (memoir, link) {
+  if (!memoir) throw new Error
+  let submemoir = memoir
+  link.forEach(function (linkLetter, index) {
+    if (index === link.length - 1) {
+      return // skip last
+    }
+    if (!submemoir[linkLetter]) {
+      submemoir[linkLetter] = {}
+    }
+    submemoir = submemoir[linkLetter]
+  }.bind(this))
+  submemoir[link[link.length - 1]] |= 0
+  submemoir[link[link.length - 1]] += 1
+}
---
+++ test/digest.js
@@ -0,0 +1,100 @@
+let test = require('tape')
+let digestWord = require('../src/digest').digestWord
+let incrementSubmemoir = require('../src/digest').incrementSubmemoir
+
+var fixtures = [
+  {
+    expectedMemoir: {
+      'START': {
+        'START': {
+          'd': 4
+        },
+        'd': {
+          'o': 2,
+          'i': 1,
+          'u': 1
+        }
+      },
+      'd': {
+        'o': {
+          'g': 1,
+          't': 1
+        },
+        'i': {
+          'g': 1
+        },
+        'u': {
+          'g': 1
+        }
+      },
+      'o': {
+        'g': {
+          'END': 1
+        },
+        't': {
+          'END': 1
+        }
+      },
+      'i': {
+        'g': {
+          'END': 1
+        }
+      },
+      'u': {
+        'g': {
+          'END': 1
+        }
+      }
+    },
+    words: ['dog', 'dig', 'dug', 'dot'],
+    linkLength: 2
+  }
+]
+
+function testDigest (expectedMemoir, words, linkLength, memoir, t) {
+  t.plan(1)
+  memoir = memoir || {}
+  if (!memoir) throw new Error
+  words.forEach(function (word) {
+    digestWord(memoir, word)
+  })
+  t.deepEqual(memoir, expectedMemoir)
+}
+
+test('digest', function (t) {
+  fixtures.forEach(function (fixture) {
+    t.test(
+      'should correctly digest ' + JSON.stringify(fixture.words),
+      testDigest.bind(null, fixture.expectedMemoir, fixture.words, fixture.linkLength, fixture.memoir)
+    )
+  })
+})
+
+test('incrementSubmemoir', function (t) {
+  [
+    {
+      memoir: {},
+      link: ['d', 'o', 'g'],
+      expectedMemoir: {'d': {'o': {'g': 1}}}
+    },
+    {
+      memoir: {'d': {'o': {'g': 1}}},
+      link: ['d', 'o', 't'],
+      expectedMemoir: {'d': {'o': {'g': 1,'t': 1}}}
+    },
+    {
+      memoir: {'d': {'o': {'g': 1}}},
+      link: ['d', 'u', 'g'],
+      expectedMemoir: {'d': {'o': {'g': 1},'u': {'g': 1}}}
+    }
+  ].forEach(function (fixture) {
+    t.test(
+      'should incrementSubmemoir ' + JSON.stringify(fixture.link),
+      function (t) {
+        t.plan(1)
+        incrementSubmemoir(fixture.memoir, fixture.link)
+        t.deepEqual(fixture.memoir, fixture.expectedMemoir)
+      }
+    )
+  })
+})
