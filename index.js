e916fedcf2a18da281504867ae8909cfb46897c0
Aaron J. Lang <aaronjameslang@googlemail.com>
2016-08-17 08:48:14 +0100

    Refactor

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const R = require('ramda')
+
 const generateUniqueWord = require('./src/generate').generateUniqueWord
 const digestWord = require('./src/digest').digestWord
 
@@ -17,11 +19,10 @@ function generateMemoirFromWords (wordsIn) {
 function generateWordsFromMemoir (memoir, number, wordsToExclude, random) {
   random = random || Math.random
   if (!wordsToExclude.has) wordsToExclude = new Set(wordsToExclude)
-  let wordsOut = []
-  for (let i = 0; i < number; i += 1) {
+  const wordsOut = R.times(() => {
     const word = generateUniqueWord(memoir, random, wordsToExclude)
-    wordsOut.push(word)
     wordsToExclude.add(word)
-  }
+    return word
+  }, number)
   return wordsOut
 }
