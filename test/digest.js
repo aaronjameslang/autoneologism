f6a5cd63436e3d5779acbf0c9df643a6a985deee
Aaron J. Lang <aaronjameslang@googlemail.com>
2016-09-06 16:19:27 +0100

    Indent

--- test/digest.js
+++ test/digest.js
@@ -1,29 +1,29 @@
 const test = require('tape')
 const incrementSubmemoir = require('../src/digest')._incrementSubmemoir
 
-  ;[
-    {
-      memoir: {},
-      link: ['d', 'o', 'g'],
-      expectedMemoir: {'d': {'o': {'g': 1}}}
-    },
-    {
-      memoir: {'d': {'o': {'g': 1}}},
-      link: ['d', 'o', 't'],
-      expectedMemoir: {'d': {'o': {'g': 1, 't': 1}}}
-    },
-    {
-      memoir: {'d': {'o': {'g': 1}}},
-      link: ['d', 'u', 'g'],
-      expectedMemoir: {'d': {'o': {'g': 1}, 'u': {'g': 1}}}
+;[
+  {
+    memoir: {},
+    link: ['d', 'o', 'g'],
+    expectedMemoir: {'d': {'o': {'g': 1}}}
+  },
+  {
+    memoir: {'d': {'o': {'g': 1}}},
+    link: ['d', 'o', 't'],
+    expectedMemoir: {'d': {'o': {'g': 1, 't': 1}}}
+  },
+  {
+    memoir: {'d': {'o': {'g': 1}}},
+    link: ['d', 'u', 'g'],
+    expectedMemoir: {'d': {'o': {'g': 1}, 'u': {'g': 1}}}
+  }
+].forEach(fixture => {
+  test.test(
+    'should incrementSubmemoir ' + JSON.stringify(fixture.link),
+    t => {
+      t.plan(1)
+      incrementSubmemoir(fixture.memoir, fixture.link)
+      t.deepEqual(fixture.memoir, fixture.expectedMemoir)
     }
-  ].forEach(fixture => {
-    test.test(
-      'should incrementSubmemoir ' + JSON.stringify(fixture.link),
-      t => {
-        t.plan(1)
-        incrementSubmemoir(fixture.memoir, fixture.link)
-        t.deepEqual(fixture.memoir, fixture.expectedMemoir)
-      }
-    )
-  })
+  )
+})
