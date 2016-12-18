bb7e11b7a0ae47963c80605887b101705b638e32
Aaron J. Lang <aaronjameslang@googlemail.com>
2016-12-18 19:32:41 +0000

    Fix jsdoc comments

--- src/generate.js
+++ src/generate.js
@@ -10,13 +10,13 @@ module.exports = {
 /**
  * Generate words from a memoir describing what the output should look like
  *
- * @param {{}} Memoir, an object describing what the output words should be like
+ * @param {{}} memoir, an object describing what the output words should be like
  * @param {int} linkLength The link length used to generate the memoir
  * @param {int} [maxNumberOfWords] The number of words to output, generation may stop before this if maxNumberOfAttempts is reached, 100 by default
  * @param {string[]} wordsToExclude Words to include from the output, can be empty
  * @param {function} [random] Function returning ints between 0 and 1, default is Math.random
  * @param {int} [maxNumberOfAttempts] Maximum number of attempts, an attempt fails if it generates a word in wordsToExclude, default is 100*maxNumberOfWords
- * @return {{attempts: int, efficiency: float, words: string[]}}
+ * @returns {{attempts: int, efficiency: float, words: string[]}}
  */
 function generateWordsFromMemoir (memoir, linkLength, maxNumberOfWords, wordsToExclude, random, maxNumberOfAttempts) {
   maxNumberOfWords = maxNumberOfWords || 100
@@ -47,9 +47,10 @@ function generateWordsFromMemoir (memoir, linkLength, maxNumberOfWords, wordsToE
 /**
  * Generate a word from a memoir
  *
- * @param {{}} Memoir, an object describing what the output words should be like
+ * @param {{}} memoir, an object describing what the output words should be like
  * @param {int} linkLength The link length used to generate the memoir
  * @param {function} random Function returning ints between 0 and 1
+ * @returns {string}
  */
 function generateWord (memoir, linkLength, random) {
   let word = ''
@@ -64,10 +65,11 @@ function generateWord (memoir, linkLength, random) {
 /**
  * Generate the next letter in the word, based on the memoir and the letters generated so far
  *
- * @param {string} wordSoFar The begining of the word, as much as has been built so far
+ * @param {string} wordSoFar The beginning of the word, as much as has been built so far
  * @param {{}} memoir The memoir describing the options for the next letter
  * @param {int} linkLength The linkLength used to generate the memoir
- * @param {int} index A nuber between 0 and 1, used to select which letter to choose
+ * @param {int} index A number between 0 and 1, used to select which letter to choose
+ * @returns {string} A single character
  */
 function calculateNextLetter (wordSoFar, memoir, linkLength, index) {
   const link = wordSoFar.slice(-linkLength + 1).split('')
@@ -83,6 +85,7 @@ function calculateNextLetter (wordSoFar, memoir, linkLength, index) {
  *
  * @param {array} memoirLeaf An array of letter probabilites
  * @param {int} index The value used to select a letter from the options
+ * @returns {string} A single character
  */
 function findLetterInMemoirLeaf (memoirLeaf, index) {
   const total = memoirLeaf[memoirLeaf.length - 2]
